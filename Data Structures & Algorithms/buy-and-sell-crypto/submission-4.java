4f5f7c58114fea663daa319ddfe6f4837188bc58
Zé Poças <133214526+zepocas@users.noreply.github.com>
2026-05-04 15:50:37 +0100

    Add: buy-and-sell-crypto - submission-4

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-4.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int min = 101, best = 0;
+
+        for (int p : prices) {
+            min = Math.min(min, p);
+            best = Math.max(best, p - min);
+        }
+        
+        return best;
+    }
+}
