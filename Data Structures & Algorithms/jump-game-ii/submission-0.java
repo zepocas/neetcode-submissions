18868bf967854db0c4aa664f627e3d28f4e0119d
Zé Poças <133214526+zepocas@users.noreply.github.com>
2026-05-07 22:13:32 +0100

    Add: jump-game-ii - submission-0

---
+++ Data Structures & Algorithms/jump-game-ii/submission-0.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int jump(int[] nums) {
+        int res = 0;
+        int l = 0, r = 0;
+
+        while (r < nums.length - 1) {
+            int farthest = 0;
+            for (int i = l; i <= r; i++) {
+                farthest = Math.max(farthest, i + nums[i]);
+            }
+            
+            l = r + 1;
+            r = farthest;
+            res++;
+        }
+        
+        return res;
+    }
+}
