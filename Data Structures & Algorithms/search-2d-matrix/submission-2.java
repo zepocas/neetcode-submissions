85fb79de7f2b5f3033394d2042efcf9211bbcb4a
Zé Poças <133214526+zepocas@users.noreply.github.com>
2026-05-14 00:01:35 +0100

    Add: search-2d-matrix - submission-2

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-2.java
@@ -0,0 +1,44 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int b = 0, u = matrix.length - 1;
+        int rowLen = matrix[0].length - 1;
+
+        while (b <= u) {
+            int mid = (b+u) / 2;
+
+            if (matrix[mid][0] > target) {
+                u = mid - 1;
+                continue;
+            } else if (matrix[mid][0] < target) {
+                if (matrix[mid][rowLen] >= target) {
+                    return binarySearchInArray(matrix[mid], target);
+                } else {
+                    b = mid + 1;
+                    continue;
+                }
+            } else {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+
+    public boolean binarySearchInArray(int[] array, int target) {
+        int l = 0, r = array.length - 1;
+
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            
+            if (array[mid] > target) {
+                r = mid - 1;
+            } else if (array[mid] < target) {
+                l = mid + 1;
+            } else {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
