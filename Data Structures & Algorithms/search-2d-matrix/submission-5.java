0a8529cc0e7e43e5c5d86f907cfa0315beafdce6
Zé Poças <133214526+zepocas@users.noreply.github.com>
2026-06-24 17:38:45 +0100

    Add: search-2d-matrix - submission-5

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-5.java
@@ -0,0 +1,39 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        int top = 0;
+        int bot = rows - 1;
+
+        while (top <= bot) {
+            int row = (top + bot) / 2;
+            if (target > matrix[row][cols-1]) {
+                top = row + 1;
+            } else if (target < matrix[row][0]) {
+                bot = row - 1;
+            } else {
+                break;
+            }
+        }
+
+        if (!(top <= bot)) {
+            return false;
+        }
+
+        int row = (top + bot) / 2;
+        int l = 0, r = cols - 1;
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            if (matrix[row][mid] > target) {
+                r = mid - 1;
+            } else if (matrix[row][mid] < target) {
+                l = mid + 1;
+            } else {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
